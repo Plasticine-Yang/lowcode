76c71759dcb4ab32cf319ac8634f891eee59cbba
plasticine <975036719@qq.com>
2022-07-26 23:01:01 +0800

    refactor(basic-layout): 分离基本布局到各个单独组件中

--- packages/core/src/types/components.d.ts
+++ packages/core/src/types/components.d.ts
@@ -8,10 +8,16 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     BasicLayout: typeof import('./../layouts/BasicLayout.vue')['default']
+    BasicLayoutHeader: typeof import('./../layouts/basic-layout/components/BasicLayoutHeader.vue')['default']
+    Drawer: typeof import('./../layouts/basic-layout/components/drawer-panel/Drawer.vue')['default']
+    DrawerPanel: typeof import('./../layouts/basic-layout/components/drawer-panel/index.vue')['default']
+    DrawerPanelHeader: typeof import('./../layouts/basic-layout/components/drawer-panel/DrawerPanelHeader.vue')['default']
+    FieldPanel: typeof import('./../layouts/basic-layout/components/field-panel/index.vue')['default']
     NButton: typeof import('naive-ui')['NButton']
     NLayout: typeof import('naive-ui')['NLayout']
     NLayoutContent: typeof import('naive-ui')['NLayoutContent']
     NLayoutHeader: typeof import('naive-ui')['NLayoutHeader']
     NLayoutSider: typeof import('naive-ui')['NLayoutSider']
+    SettingPanel: typeof import('./../layouts/basic-layout/components/setting-panel/index.vue')['default']
   }
 }
