b438c9b6a7613b28161d742808bded94aaa15204
plasticine <975036719@qq.com>
2022-07-28 11:29:12 +0800

    feat(core): 定义drawer中draggable的ghost-class和编写相关样式

--- packages/core/src/layouts/basic-layout/components/drawer-panel/Drawer.vue
+++ packages/core/src/layouts/basic-layout/components/drawer-panel/Drawer.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const drawerItemList = reactive<any[]>([])
+</script>
+
 <template>
-  <section class="wh-full flex-center bg-white">drawer</section>
+  <draggable
+    :list="drawerItemList"
+    item-key="id"
+    group="basic-field"
+    class="wh-full bg-white"
+    ghost-class="ghost"
+    :animation="300"
+  >
+    <template #item="{ element }">
+      <div class="cursor-pointer">{{ element.fieldName }}</div>
+    </template>
+  </draggable>
 </template>
