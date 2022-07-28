a25f33fd743e27abf4472e36551f34de461d0c7c
skywynne <taiiiyang@outlook.com>
2022-07-28 23:32:51 +0800

    feat: 增加单行输入组件可以渲染到画布，事件总线注入到全局，可以利用事件总线进行配置

--- packages/core/src/layouts/basic-layout/components/setting-panel/index.vue
+++ packages/core/src/layouts/basic-layout/components/setting-panel/index.vue
@@ -1,3 +1,15 @@
 <template>
   <section class="wh-full flex-center">setting panel</section>
 </template>
+
+<script lang="ts" setup>
+import { ref, inject } from 'vue'
+import EventBus from '@/lib/bus'
+
+const currentWidget = ref()
+// 注入事件总线
+const $bus = inject('$bus') as EventBus
+$bus.on('changeCurrentEdit', (evt: Event) => {
+  console.log(evt, 'evt')
+})
+</script>
