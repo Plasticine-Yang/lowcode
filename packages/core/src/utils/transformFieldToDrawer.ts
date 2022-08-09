60f3e0df11fc20635c496f61442b6fa1da7ca503
ChenLlting <mouche_2021@qq.com>
2022-08-09 14:54:42 +0800

    feat: 增加容器栅格

--- packages/core/src/utils/transformFieldToDrawer.ts
+++ packages/core/src/utils/transformFieldToDrawer.ts
@@ -19,6 +19,7 @@ export default function transformFieldToDrawer(field: IField): DrawerComponent {
     componentProps: fieldcopy.componentProps,
     componentPropsMeta: fieldcopy.componentPropsMeta,
     dragHandlerName: fieldcopy.fieldName,
+    children: fieldcopy.children,
   }
 
   return drawerComponent
