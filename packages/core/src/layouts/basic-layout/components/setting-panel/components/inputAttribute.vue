beebd0f5c3e99835952d64e68fa274159bf74c6b
ChenLlting <mouche_2021@qq.com>
2022-08-18 17:37:07 +0800

    chore: 移除无用console.log

--- packages/core/src/layouts/basic-layout/components/setting-panel/components/inputAttribute.vue
+++ packages/core/src/layouts/basic-layout/components/setting-panel/components/inputAttribute.vue
@@ -21,7 +21,7 @@ export default {
   setup(props, ctx) {
     function iptChange(e) {
       // ctx 这里通过emit来修改父组件中v-model的数据
-      console.log(e)
+
       ctx.emit('update:propsModel', e)
     }
     return {
