25ed519d32db5ccb0c6f78c3bbc00468bd00a62f
ChenLlting <mouche_2021@qq.com>
2022-08-20 14:29:53 +0800

    chore: 移除transformFieldToDrawer文件中遗留的无用代码

--- packages/core/src/utils/transformFieldToDrawer.ts
+++ packages/core/src/utils/transformFieldToDrawer.ts
@@ -5,14 +5,6 @@ import { generateId } from './generateId'
  */
 export default function transformFieldToDrawer(field: IField): DrawerComponent {
   const fieldcopy = JSON.parse(JSON.stringify(field))
-  //如果是grid栅格的话，其children应该为n个子数组
-  if (fieldcopy.componentName == 'drawerGrid') {
-    //这里不能使用fill初始化数组，二维数组初始化时，fill填充的为对象类型（数组）为浅拷贝
-    fieldcopy.children = new Array(fieldcopy.componentProps!.cols)
-    for (let i = 0; i < fieldcopy.children.length; i++) {
-      fieldcopy.children[i] = []
-    }
-  }
   const drawerComponent: DrawerComponent = {
     id: generateId(),
     componentName: fieldcopy.componentName,
