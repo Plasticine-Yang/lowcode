e6d299ff056df8809465a4e02b67e08e6f5fbc84
ChenLlting <mouche_2021@qq.com>
2022-08-22 14:03:40 +0800

    chore: 修改导入示例

--- packages/core/src/layouts/basic-layout/components/drawer-panel/drawer-header/import-export/showExampe.ts
+++ packages/core/src/layouts/basic-layout/components/drawer-panel/drawer-header/import-export/showExampe.ts
@@ -1,40 +1,58 @@
-interface typeShowExample {
-  id: string
-  componentName: string
-  componentProps: Props
-  componentPropMeta: object
-  dragHandlerName?: string
-}
-interface Props {
-  type?: string
-  clearable?: boolean
-  readonly?: boolean
-  disabled?: boolean
-}
-
-export const showExample: typeShowExample[] = [
+export const showExample = [
   {
-    id: '1',
-    componentName: 'n-input',
+    id: '427a91a1-a03f-4470-a722-142bdeba9918',
+    componentName: 'DrawerInput',
     componentProps: {
       type: 'text',
       clearable: false,
-      readonly: false,
-      disabled: false,
+    },
+    componentPropsMeta: {
+      placeholder: {
+        name: '提示信息',
+        fieldComponent: 'input-attribute',
+      },
+      clearable: {
+        name: '是否可清除',
+        fieldComponent: 'n-switch',
+      },
     },
     dragHandlerName: '单行输入',
-    componentPropMeta: {
-      clearable: {
-        name: '可清除',
-        fieldComponent: 'n-switch',
+    //类型
+    type: 'basic-senior',
+    style: '',
+    //事件属性
+    eventProps: {
+      onFocus: {
+        name: 'onFocus',
+        code: 'console.log("focus")',
+        eventHandlerGenerator: '(code) => (e) => eval(code)',
+        eventHandlerArgsDescriptor: {
+          e: 'Event',
+        },
       },
-      readonly: {
-        name: '只读',
-        fieldComponent: 'n-switch',
+      onBlur: {
+        name: 'onBlur',
+        code: 'console.log("blur")',
+        eventHandlerGenerator: '(code) => (e) => eval(code)',
+        eventHandlerArgsDescriptor: {
+          e: 'Event',
+        },
       },
-      disabled: {
-        name: '禁用',
-        fieldComponent: 'n-switch',
+      onChange: {
+        name: 'onChange',
+        code: 'console.log("change")',
+        eventHandlerGenerator: '(code) => (e) => eval(code)',
+        eventHandlerArgsDescriptor: {
+          e: 'Event',
+        },
+      },
+      onInput: {
+        name: 'onInput',
+        code: 'console.log("input")',
+        eventHandlerGenerator: '(code) => (e) => eval(code)',
+        eventHandlerArgsDescriptor: {
+          e: 'Event',
+        },
       },
     },
   },
